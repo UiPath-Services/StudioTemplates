c7872316b568b71394e6268b06f07c9c0ff31173
TeodoraPatrascu <teodora.patrascu@uipath.com>
2022-08-31 13:04:41 +0300

    First try at fixtures

--- DocumentUnderstandingProcess/Tests/PythonTests/TemplateJson/test_template_json.py
+++ DocumentUnderstandingProcess/Tests/PythonTests/TemplateJson/test_template_json.py
@@ -5,12 +5,14 @@ from pytest import mark
 @mark.template_json
 class TemplateJsonTests:
 
-    @mark.vb
+    @mark.parametrize("dependency_check",
+             ["./../DocumentUnderstandingProcess/contentFiles/any/any/pt0/VisualBasic/project.json",
+              "../../DocumentUnderstandingProcess/contentFiles/any/any/pt0/.local/template.json"]
+             )
     def test_template_json_dependencies_vb(self, dependency_check):
-        dependency_check("asd.json", "project.json")
-        assert True
+        assert dependency_check()
 
-    @mark.csharp
-    def test_template_json_dependencies_csharp(self, dependency_check):
-        dependency_check("asd.json", "project.json")
-        assert True
+    # @mark.csharp
+    # def test_template_json_dependencies_csharp(self, dependency_check):
+    #     dependency_check("../../DocumentUnderstandingProcess/contentFiles/any/any/pt1/CSharp/project.json", "../../DocumentUnderstandingProcess/contentFiles/any/any/pt1/.local/template.json")
+    #     assert True
--- DocumentUnderstandingProcess/Tests/conftest.py
+++ DocumentUnderstandingProcess/Tests/conftest.py
@@ -1,9 +1,24 @@
 from pytest import fixture
+import json
 
 
 # function, class, module, package, session
 @fixture(scope='function')
 def dependency_check(template_json, proj_json):
-    print("I have done the dependency check")
-    return True
+    vb_project_json = json.load(open(proj_json, 'r'))
+    vb_template_json = json.load(open(template_json, 'r'))
+
+    for key, value in vb_project_json.items():
+        if type(key) == str:
+            vb_project_json[key.lower()] = value
+        else:
+            vb_project_json[key] = value
+
+    for key, value in vb_template_json.items():
+        if type(key) == str:
+            vb_template_json[key.lower()] = value
+        else:
+            vb_template_json[key] = value
+
+    return vb_project_json["dependencies"] == vb_template_json["dependencies"]
 
