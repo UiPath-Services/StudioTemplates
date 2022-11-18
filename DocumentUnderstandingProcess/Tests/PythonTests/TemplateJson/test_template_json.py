aec323867c4daed6cc3cffc96edf995d4332a8e1
TeodoraPatrascu <teodora.patrascu@uipath.com>
2022-11-18 15:43:33 +0200

    [Pytest] Add test for template.json field FileInfoCollection

--- DocumentUnderstandingProcess/Tests/PythonTests/TemplateJson/test_template_json.py
+++ DocumentUnderstandingProcess/Tests/PythonTests/TemplateJson/test_template_json.py
@@ -34,3 +34,22 @@ class TemplateJsonTests:
         data = load_json(app_constants.TEMPLATE_JSON)
 
         assert app_constants.MAIN_ACTION_CENTER in data["MainFile"]
+
+    @staticmethod
+    def test_template_json_fileinfocollection(convert_to_lower, load_json, app_constants):
+        """
+        alpha.uipath.com/dualphatests
+        Test Manager project: Document Understanding Process
+        Test Case: DUP56
+
+        Check if the FileInfoCollection field, which holds all the configured RPA tests,
+        between project.json and template.json is the same
+        """
+        vb_project_json = load_json(app_constants.PROJECT_JSON)
+        vb_template_json = load_json(app_constants.TEMPLATE_JSON)
+
+        vb_project_json = convert_to_lower(vb_project_json)
+        vb_template_json = convert_to_lower(vb_template_json)
+        vb_project_json_design_options = convert_to_lower(vb_project_json["designoptions"])
+
+        assert vb_project_json_design_options["fileinfocollection"] == vb_template_json["fileinfocollection"]
