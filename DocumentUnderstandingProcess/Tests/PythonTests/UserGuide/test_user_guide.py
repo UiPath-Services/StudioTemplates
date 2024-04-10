a54ff3302ff68c53b49de6c07bd57d33a3cd255b
Sergiu Wittenberger <sergiu.wittenberger@uipath.com>
2024-04-10 15:48:48 +0300

    fixed the test. It seems the extraction adds a blank space between the number and the -preview line.

--- DocumentUnderstandingProcess/Tests/PythonTests/UserGuide/test_user_guide.py
+++ DocumentUnderstandingProcess/Tests/PythonTests/UserGuide/test_user_guide.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest import mark
 import re
 
@@ -21,10 +22,10 @@ class UserGuideTests:
 
         # Extract text
         output = user_guide.pages[4].extract_text()
-        output = output.split("\n")[-1].replace("  ", "|")
-
+        output = output.split("\n")[-1].replace("  ", "|").replace(" ", "")
         # check for the presence of the release version in the user guide
         found = re.search(test_data["release_version"], output) is not None
+
         should_pass = test_data["expected_result"] == "pass"
 
         # asserting the presence of the release version in the user guide, considering also the expected_result
