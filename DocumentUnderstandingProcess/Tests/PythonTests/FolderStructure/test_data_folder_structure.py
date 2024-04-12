5029fdf77a4c73b3c3d7291a822f913e2b9a9404
swittenberger <28090775+swittenberger@users.noreply.github.com>
2024-04-12 10:40:42 +0300

    [Pytest] Take changes from PR #223 (#272) (#273)
    * Changes from PR #223 [Pytest]
    
    * Pytests corrections and package updates
    
    * fixed the test. It seems the extraction adds a blank space between the number and the -preview line.
    
    * cliVersion changed in DU-Test.yml
    
    ---------
    
    Co-authored-by: TeodoraPatrascu <104830515+TeodoraPatrascu@users.noreply.github.com>
    Co-authored-by: Sergiu Wittenberger <sergiu.wittenberger@uipath.com>

--- DocumentUnderstandingProcess/Tests/PythonTests/FolderStructure/test_data_folder_structure.py
+++ DocumentUnderstandingProcess/Tests/PythonTests/FolderStructure/test_data_folder_structure.py
@@ -7,26 +7,34 @@ import os
 @mark.data_folder_structure
 class DataFolderTests:
     @staticmethod
-    def test_data_folder_structure(app_constants, get_test_data_folder):
+    def test_data_folder_structure(app_constants, test_data):
         """
+        alpha.uipath.com/dualphatests
+        Test Manager project: Document Understanding Process
+        Test Case: DUP29
+
         Check the structure of the Data folder and its inner folders.
         """
         # count folders/files in Data folder
-        number_of_files = len(os.listdir(app_constants.PROJECT + get_test_data_folder["folderPath"]))
+        number_of_files = len(os.listdir(app_constants.PROJECT + test_data["folderPath"]))
 
         # expected folders/files count in Data folder
-        expected_file_count = len(get_test_data_folder["FolderStructure"])
+        expected_file_count = len(test_data["FolderStructure"])
 
         # compare number of files with expected number of files
         assert number_of_files == expected_file_count
 
     @staticmethod
-    def test_data_folder_content(app_constants, get_test_data_folder):
+    def test_data_folder_content(app_constants, test_data):
         """
+        alpha.uipath.com/dualphatests
+        Test Manager project: Document Understanding Process
+        Test Case: DUP30
+
         Check the content of the Data folder and its inner folders.
         """
         # get the folder content
-        files = os.listdir(app_constants.PROJECT + get_test_data_folder["folderPath"])
+        files = os.listdir(app_constants.PROJECT + test_data["folderPath"])
 
         # compare folder content with the expected files
-        assert all(file in files for file in get_test_data_folder["FolderStructure"])
+        assert all(file in files for file in test_data["FolderStructure"])
--- DocumentUnderstandingProcess/Tests/PythonTests/ProjectJson/test_project_json.py
+++ DocumentUnderstandingProcess/Tests/PythonTests/ProjectJson/test_project_json.py
@@ -6,23 +6,57 @@ from pytest import mark
 @mark.project_json
 class ProjectJsonTests:
     @staticmethod
-    def test_project_json_variation_path(get_test_data_project_json, app_constants):
+    def test_project_json_variation_path(test_data, app_constants):
         """
-        Checks if the there are mentions of the .variation folder in the project.json
+        alpha.uipath.com/dualphatests
+        Test Manager project: Document Understanding Process
+        Test Case: DUP37
+
+        Checks if there are mentions of the .variation folder in the project.json
         """
-        data = yaml.safe_load((open(app_constants.PROJECT + "\\project.json", "r")))
+        data = yaml.safe_load(open(app_constants.PROJECT + "project.json"))
         # print(get_test_data.load_data(app_constants.PROJECT_JSON_TEST_DATA))
         #
         for element in data["designOptions"]["fileInfoCollection"]:
             # check for the Key presence
-            assert not get_test_data_project_json["key"] in element
+            assert not test_data["key"] in element
             # assert not get_test_data(app_constants.PROJECT_JSON_TEST_DATA)["key"] in element
 
     @staticmethod
     def test_project_json_main_file(app_constants):
         """
+        alpha.uipath.com/dualphatests
+        Test Manager project: Document Understanding Process
+        Test Case: DUP38
+
         Checks if the main file is Main-ActionCenter
         """
         data = yaml.safe_load((open(app_constants.PROJECT + "\\project.json", "r")))
 
         assert app_constants.MAIN_ACTION_CENTER in data["main"]
+
+    @staticmethod
+    def test_project_json_starts_in_background(app_constants):
+        """
+        alpha.uipath.com/dualphatests
+        Test Manager project: Document Understanding Process
+        Test Case: DUP43
+
+        Checks if the "Starts in Background" property is disabled.
+        """
+        data = yaml.safe_load((open(app_constants.PROJECT + "\\project.json", "r")))
+
+        assert data["runtimeOptions"]["requiresUserInteraction"]
+
+    @staticmethod
+    def test_project_json_supports_persistance(app_constants):
+        """
+        alpha.uipath.com/dualphatests
+        Test Manager project: Document Understanding Process
+        Test Case: DUP44
+
+        Checks if the "Supports Persistence" property is enabled.
+        """
+        data = yaml.safe_load((open(app_constants.PROJECT + "\\project.json", "r")))
+
+        assert data["runtimeOptions"]["supportsPersistence"]
